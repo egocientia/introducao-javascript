00d630e865907da767292fcd3cacc5f32a1190fc
Bruno Henrique <brunosh.contato@gmail.com>
2023-01-28 14:32:11 -0300

    organizando codigo

--- js/form.js
+++ js/form.js
@@ -5,30 +5,51 @@ botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    // extraindo inform
+    var pacienteForm = obtemPacDoForm(form)
 
-    var pacienteTr = document.createElement("tr")
+    //cria a tr e a td do paciente
+    var pacienteTr = montaTr(pacienteForm)
 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
+    //adicionando o paciente na tabela.
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
+
+    form.reset();
 })
+
+function obtemPacDoForm(form){
+
+    var pacienteForm = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return pacienteForm
+
+}
+
+function montaTr(pacienteForm){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+    
+    pacienteTr.appendChild(montaTd(pacienteForm.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(pacienteForm.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(pacienteForm.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(pacienteForm.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(pacienteForm.imc, "info-imc"));
+
+    return pacienteTr
+}
+
+function montaTd (dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td
+}
